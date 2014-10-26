dba25e0a33c2e9825ba2e7123a14b2e57b8c9179
hvozdulych <hvozdulych@gmail.com>
2014-10-26 04:22:02 +0200

    add ability to  edit and remove words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -11,6 +11,10 @@ class WordsController < ApplicationController
 		@word = Word.new
 	end
 
+	def edit
+  	  @word = Word.find(params[:id])
+	end
+
 	def create
 	  @word = Word.new(word_params)
 	 
@@ -20,12 +24,27 @@ class WordsController < ApplicationController
 	    render 'new'
 	  end
 	end
+
+	def update
+	  @word = Word.find(params[:id])
+	 
+	  if @word.update(word_params)
+	    redirect_to @word
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+	  @word = Word.find(params[:id])
+	  @word.destroy
+	 
+	  redirect_to words_path
+	end
  
 	private
 	  def word_params
 	    params.require(:word).permit(:meaning, :spelling)
 	  end
-
-
 
 end
