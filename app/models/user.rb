cae5be5cb169c34e8ea872de98a9ba02eb3d7b40
hvozdulych <hvozdulych@gmail.com>
2014-11-09 23:38:22 +0200

    fix registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :name, :surname, :email, :password, :password_confirmation, :remember_me
-  validates :name, :surname, presence: true
 end
